66a0f07484ecd7cb30163512410f20e49d6145b9
TopDriveGroup <carlos.reyes@topdrivegroup.com>
2024-08-12 19:00:37 -0500

    Se crea navegacion del modulo SIG de Financiero

--- src/pages/Landing/01NavBar/01SIG/04Finantial/01FinantialPolicies/FinantialPoliciesPage.tsx
+++ src/pages/Landing/01NavBar/01SIG/04Finantial/01FinantialPolicies/FinantialPoliciesPage.tsx
@@ -1,16 +1,16 @@
-import NavBar from '../../../../../components/LandingPage/00NavBar/NavBar';
-import SideBarQualityAdministrative from '../../../../../components/LandingPage/00NavBar/01SIG/03QualityAdministrative/SideBarQualityAdministrative/SideBarQualityAdministrative';
-import Footer from '../../../../../components/LandingPage/Footer/Footer';
+import NavBar from '../../../../../../components/LandingPage/00NavBar/NavBar';
+import SideBarFinantial from '../../../../../../components/LandingPage/00NavBar/01SIG/04Finantial/SideBarFinantial/SideBarFinantial';
+import Footer from '../../../../../../components/LandingPage/Footer/Footer';
 import styles from './styles.module.css';
 
-function QualityAdministrativePage() {
+function FinantialPoliciesPage() {
     return (
         <div className={`${styles.container} d-flex flex-column align-items-center justify-content-between`}>
             <NavBar />
             <div className={`${styles.container__Component} d-flex`}>
-                <SideBarQualityAdministrative />
+                <SideBarFinantial />
                 <div className={`${styles.component} p-3`}>
-                    <h2 className={`${styles.title} `}>Calidad y Administración</h2>
+                    <h2 className={`${styles.title} `}>Políticas de Financiero</h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt ad voluptates culpa cupiditate placeat eos possimus nostrum excepturi, dolore inventore fugiat, asperiores corporis at perferendis sequi eius aliquam ratione facilis?</p>
                 </div>
             </div>
@@ -19,4 +19,4 @@ function QualityAdministrativePage() {
     );
 }
 
-export default QualityAdministrativePage;
+export default FinantialPoliciesPage;
